04a8690345f00376954036684f2ff28f24005a4a
Nicola <va3nky@hotmail.com>
2018-10-23 19:07:20 -0400

    created logout button and created the ajax request

--- src/Top-navbar.jsx
+++ src/Top-navbar.jsx
@@ -2,9 +2,19 @@ import React, {Component} from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import UserLogin from './UserLogin';
 import UserSignUp from './UserSignUp';
+import axios from 'axios';
 import './styles/top-navbar.scss';
 
 class TopNavbar extends Component {
+
+  _handleLogout = () => {
+    axios.get('/logout')
+      .then((response) => {
+        console.log(response);
+        window.alert('logout successful')
+      })
+  }
+
   render() {
     return (
       <Navbar default collapseOnSelect id="top-nav">
@@ -21,6 +31,9 @@ class TopNavbar extends Component {
             </NavItem>
             <NavItem eventKey={2}>
               <UserLogin />
+            </NavItem>
+            <NavItem onClick={this._handleLogout} eventKey={1} >
+              Logout
             </NavItem>
           </Nav>
         </Navbar.Collapse>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -15,4 +15,8 @@ module.exports = function (app) {
   app.use(proxy('/api/itinerary', {
     target: 'http://localhost:3000/'
   }));
+
+  app.use(proxy('/logout', {
+    target: 'http://localhost:3000/'
+  }));
 }
